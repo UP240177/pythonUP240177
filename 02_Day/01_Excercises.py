b4f7948a9b6eeba5d32ae1353b4ba32af3822ee8
UP240177 <up240177@alumnos.upa.edu.mx>
2025-03-01 21:42:42 -0600

    Add files via upload

---
+++ 02_Day/01_Excercises.py
@@ -0,0 +1,21 @@
+def circleArea(radio):
+    area = math.pi*(radio**2)
+    print(f"EL area es de {area}")
+    return area
+
+def circleCircunference(radio):
+    circunf = 2*math.pi*radio
+    print(f"La circunferencia es de {circunf}")
+    return circunf
+
+firstName = "Miguel"
+lastName = "Trinidad"
+fullName = "Miguel Enrique Trinidad"
+country = "México"
+city = "Aguascalientes"
+age = 18
+year = 2006
+isMarried = False
+isTrue = True
+isLight = False
+a, b, c = "a", 1, True
---
+++ 02_Day/02_Excercises.py
@@ -0,0 +1,37 @@
+print(type(firstName), " ", len(firstName))
+print(type(lastName), " ", len(lastName))
+print(type(fullName))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(isMarried))
+print(type(isTrue))
+print(type(isLight))
+print(type(a))
+print(type(b))
+print(type(c))
+
+n1 = 5
+n2 = 4
+
+suma = n1 +n2
+diff = n2 - n1
+prod = n2 * n1
+div = n1 / n2
+remainder = n2 % n1
+exp = n1 ** n2
+floorDivision = n1 // n2
+
+r = 30
+area_of_circle = circleArea(r)
+circum_of_circle = circleCircunference(r)
+
+r = float(input("Dame el radio del circulo: "))
+area_of_circle = circleArea(r)
+circum_of_circle = circleCircunference(r)
+
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+pais = input("Ingrese su país: ")
+edad = input("Ingrese su edad: ")
